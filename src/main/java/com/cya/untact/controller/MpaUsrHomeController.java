bbf4053ee67d943353e68ccd6a57150bee9d5448
duddk1551 <duddk1551@gmail.com>
2022-02-09 21:34:49 +0900

    add jsp setting and css,js file

---
+++ src/main/java/com/cya/untact/controller/MpaUsrHomeController.java
@@ -0,0 +1,13 @@
+package com.cya.untact.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MpaUsrHomeController {
+	
+	@RequestMapping("/usr/home/main")
+	public String showMain() {
+		return "/usr/home/main";
+	}
+}
